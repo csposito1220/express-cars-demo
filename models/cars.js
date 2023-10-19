60bd0008744a37bc895e98839ce788e9d168711b
csposito1220 <csposito1220@gmail.com>
2023-10-19 14:10:32 -0400

    Make Cars

---
+++ models/cars.js
@@ -0,0 +1,26 @@
+const cars = [
+  {
+    id: 0,
+    year: "2020",
+    make: "Merc",
+    image:
+      "https://images.unsplash.com/photo-1514316454349-750a7fd3da3a?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
+    isUsed: false,
+  },
+  {
+    id: 1,
+    year: "2023",
+    make: "Chevy",
+    image:
+      "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhcnN8ZW58MHx8MHx8fDA%3D",
+    isUsed: false,
+  },
+  {
+    id: 2,
+    year: "2023",
+    make: "Porsche",
+    image:
+      "https://images.unsplash.com/photo-1503376780353-7e6692767b70?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
+    isUsed: true,
+  },
+];
